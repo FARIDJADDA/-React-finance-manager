df42d5051f817ca6ce3007657d5e05d47430835c
FARIDJADDA <faridjadda750@gmail.com>
2021-11-30 00:08:31 +0100

    Adding Conditional Content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,6 +5,7 @@ import ExpensesFilter from "./ExpensesFilter/ExpensesFilter";
 import Card from "../UI/Card";
 
 import classes from "./Expenses.module.css";
+import ExpensesList from "./ExpensesList/ExpensesList";
 
 function Expenses(props) {
   const [optionYear, setOptionYear] = useState("2021");
@@ -17,26 +18,13 @@ function Expenses(props) {
     return expense.date.getFullYear().toString() === optionYear;
   });
 
-  let noExpensesContent = <p>0 dépenses</p>;
-
-  if (filteredExpenses.length > 0) {
-    noExpensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        date={expense.date}
-        title={expense.title}
-        amount={expense.amount}
-      />
-    ));
-  }
-
   return (
     <Card className={classes.expenses}>
       <ExpensesFilter
         selected={optionYear}
         onSaveFIlterData={saveFIlterDataHandler}
       />
-      {noExpensesContent}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 }
